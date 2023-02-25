73cbc5e46fa074e5d38df833d0de137f77a08dcd
afin asgarli <afinasgarli@yandex.com>
2023-02-25 20:14:23 +0400

    Prepare a burger menu for the mobile var

--- src/laylouts/mobile-navbar/index.js
+++ src/laylouts/mobile-navbar/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { Button, Container, Image } from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Button, Container, Image, ListGroup } from 'react-bootstrap';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineMenu } from 'react-icons/ai';
+import { VscChromeClose } from 'react-icons/vsc';
+import routesData from 'data/routesData';
 import './mobile-navbar.scss';
 
 const MobileNavbar = () => {
@@ -10,27 +12,34 @@ const MobileNavbar = () => {
 
   const name = "Afin Asgarli";
 
+  const [menu, setMenu] = useState(true);
+
 
   return (
     <nav id='mobile-navbar'>
-      <Container className="d-flex justify-content-between align-items-center">
-        <Link to={""} className="navbar-brand d-flex align-items-center">
-          <Image src={"images/logos/logo_no_text.svg"} width={"60"} />
-          <div className='name d-flex'>
-            {name.split(" ").map((word, i) => <span className='word d-flex' key={i}>{word.split("")
-              .map((letter, u) => <b className='letter' key={u}>{letter}</b>)}</span>)}
+      <Container>
+        <div className='d-flex justify-content-between align-items-center'>
+          <Link to={""} className="navbar-brand d-flex align-items-center">
+            <Image src={"images/logos/logo_no_text.svg"} width={"60"} />
+            <div className='name d-flex'>
+              {name.split(" ").map((word, i) => <span className='word d-flex' key={i}>{word.split("")
+                .map((letter, u) => <b className='letter' key={u}>{letter}</b>)}</span>)}
+            </div>
+          </Link>
+          <div className="burger-menu">
+            <Button onClick={() => setMenu(prev => !prev)} className='burger-icon'>
+              <VscChromeClose className={menu ? "active" : null} />
+              <AiOutlineMenu className={!menu ? "active" : null} />
+            </Button>
           </div>
-        </Link>
-        <div className="burger-menu">
-          <Button className='burger-icon'><AiOutlineMenu width={"20px"}/></Button>
         </div>
-        {/* <ListGroup className='navbar-nav d-flex align-items-center justify-content-between'>
+        <ListGroup className={`navbar-nav d-flex flex-column menu ${menu ? "active" : null}`}>
           {routesData.map(({ path, title }) => (
             <ListGroup.Item key={title}>
               <Link to={path} className={pathname === path ? "active nav-link" : "nav-link"}>{title}</Link>
             </ListGroup.Item>
           ))}
-        </ListGroup> */}
+        </ListGroup>
       </Container>
     </nav>
   )

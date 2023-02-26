436520b2ee612c8468b53ca41b0f480f383e85f4
afin asgarli <afinasgarli@yandex.com>
2023-02-26 13:20:54 +0400

    Changing media query breakpoints

--- src/laylouts/footer/index.js
+++ src/laylouts/footer/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col, ListGroup } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import footerData from 'data/footer.json';
 import './footer.scss';
 
+
 const Footer = () => {
+  console.log(footerData);
   return (
     <footer id='footer'>
-      <Container>
-        Footer
+      <Container className="footer-top">
+        <Row className='justify-content-center'>
+          <Col lg={9} style={{ backgroundColor: "aqua" }}>
+            <Row>
+              {footerData.map(({ title, links }) => (
+                <Col lg={4} xs={6} md={6}  key={title}>
+                  <h4>{title}</h4>
+                  <ListGroup>
+                    {
+                      links.map(({name, link, icon}) =>(
+                        <ListGroup.Item key={name} >
+                          <Link to={link}>{name}</Link>
+                        </ListGroup.Item>
+                      ))
+                    }
+                  </ListGroup>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </footer>
   )
 }
-
 export default Footer;

95d1a7986ecff83b91a6cc0b506ef69cdcb02986
afin asgarli <afinasgarli@yandex.com>
2023-02-27 01:04:50 +0400

    Certificates route deletion

--- src/data/footerData.js
+++ src/data/footerData.js
@@ -23,10 +23,6 @@ export default [
             {
                 name: "Resume",
                 link: "/resume"
-            },
-            {
-                name: "Certificates",
-                link: "/"
             }
         ]
     },

a9a15de855301de6a983fb3d4432e0b29af9888f
afin asgarli <afinasgarli@yandex.com>
2023-02-28 12:41:08 +0400

    Resetting the height of the burger menu

--- src/laylouts/main/index.js
+++ src/laylouts/main/index.js
@@ -1,10 +1,12 @@
-import React, { Children } from 'react';
+import context from 'context';
+import React, { Children, useContext } from 'react';
 import { Container } from 'react-bootstrap';
 import './main.scss';
 
 const Main = ({children}) => {
+  const {menu} = useContext(context);
   return (
-    <main id='main'>{children}</main>
+    <main id='main' className={!menu ? "active" : null}>{children}</main>
   )
 }
 

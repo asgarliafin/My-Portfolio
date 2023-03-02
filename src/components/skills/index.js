0417127bc54dd7433036f6ff35d40a3965445959
afin asgarli <afinasgarli@yandex.com>
2023-03-02 23:59:19 +0400

    Skills Component preparation

--- src/components/skills/index.js
+++ src/components/skills/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
-import { Container } from 'react-bootstrap';
+import { Col, Container, Image, Row } from 'react-bootstrap';
+import SkillsData from 'data/skills.json';
+import './skills.scss';
 
 const Skills = () => {
   return (
-    <div>
+    <div id='skills'>
       <Container>
-        Skills
+        <Row className="justify-content-between">
+          <Col lg={6} style={{background : "darkred"}}>
+            <h2>I got the experience. <br /> Here is my toolbelt for success.</h2>
+          </Col>
+          <Col lg={6} style={{background : "blue"}}>
+            <Row className='skills-row'>
+            {SkillsData.map(({title, icon}) =>(
+              <Col lg={2} className="skill-item" title={title}>
+                <Image src={icon} className="icon" width={"40"}/>
+                <span className='title'>{title}</span>
+              </Col>
+            ))}
+            </Row>
+          </Col>
+        </Row>
       </Container>
     </div>
   )

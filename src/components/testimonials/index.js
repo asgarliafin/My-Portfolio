60475e052f442c829b0f14ff8a02c65603b8a3b1
afin asgarli <afinasgarli@yandex.com>
2023-03-02 14:37:16 +0400

    Preparation of testimonials

--- src/components/testimonials/index.js
+++ src/components/testimonials/index.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import { SectionTitle } from 'components';
-import testimonialsData from 'data/testimonials.json'
-import { Container } from 'react-bootstrap';
+import React from 'react';
+import TestimonialsData from 'data/testimonials.json'
+import { Container, Row, Col } from 'react-bootstrap';
+import './testimonials.scss';
 
 const Testimonials = () => {
-  return (
-    <div>
-      <Container>
-        <SectionTitle />
-        {testimonialsData.map((elm, i) => (
-          <div className='testimonialsCard' key={i}>
-            Testimonials Card
-          </div>
-        ))}
+    return (
+        <div id='testimonials'>
+            <Container>
+                <Row className='justify-content-center'>
+                    <Col lg={10} sm={12} xs={12}>
+                        <Row className='testimonials-row'>
 
-      </Container>
-    </div>
-  )
+                            {TestimonialsData.map(({ quote, name, job }) => (
+                                <Col lg={4} xs={10} sm={5} md={5}>
+                                    <div className="testimonials-card d-flex flex-column justify-content-between">
+                                        <p className='quote'>"{quote}"</p>
+                                        <p className='info'>
+                                            <b className='name'>{name}</b> - <span className='job'>{job}</span>
+                                        </p>
+                                    </div>
+                                </Col>
+                            ))}
+
+                        </Row>
+
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
 }
 
 export default Testimonials;

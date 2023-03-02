eb7697c531f5fadb4b97967057e4a5e09043b02a
afin asgarli <afinasgarli@yandex.com>
2023-03-02 15:40:09 +0400

    Add Stikers

--- src/components/testimonials/index.js
+++ src/components/testimonials/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import TestimonialsData from 'data/testimonials.json'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Image } from 'react-bootstrap';
 import './testimonials.scss';
 
 const Testimonials = () => {
@@ -11,13 +11,15 @@ const Testimonials = () => {
                     <Col lg={10} sm={12} xs={12}>
                         <Row className='testimonials-row'>
 
-                            {TestimonialsData.map(({ quote, name, job }) => (
-                                <Col lg={4} xs={10} sm={5} md={5}>
+                            {TestimonialsData.map(({ quote, name, job }, i) => (
+                                <Col lg={4} xs={10} sm={5} md={4}>
                                     <div className="testimonials-card d-flex flex-column justify-content-between">
+                                        {i === 0 && <Image src={"/images/stikers/testimonials/yay.svg"} className='yay' />}
                                         <p className='quote'>"{quote}"</p>
                                         <p className='info'>
                                             <b className='name'>{name}</b> - <span className='job'>{job}</span>
                                         </p>
+                                        {i == 2 && <Image src="/images/stikers/testimonials/squiggle2.svg" className='squiggle2'/>}
                                     </div>
                                 </Col>
                             ))}

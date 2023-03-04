16adbdd47de3a95c83b9b28a6c5cf3165790942c
afin asgarli <afinasgarli@yandex.com>
2023-03-04 22:30:32 +0400

    change favicon

--- src/components/heading/index.js
+++ src/components/heading/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import './heading.scss';
 
 const Heading = () => {
   return (
-    <div>Heading</div>
+    <div id={'heading'}>
+      <Container>
+        <Row className={'justify-content-center'}>
+          <Col lg={8}>
+            <h1>Projects</h1>
+            <p>I've built cool apps and websites using anything from HTML to React (and even PHP!). Here are some of my favorite projects over the course of my journey.</p>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 

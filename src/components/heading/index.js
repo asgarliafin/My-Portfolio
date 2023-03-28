ad02a077d7bd888a7a49db1f29891082bf0ea82c
afinasgarli <afinasgarli@yandex.com>
2023-03-28 05:17:18 +0400

    projects

--- src/components/heading/index.js
+++ src/components/heading/index.js
@@ -11,7 +11,7 @@ const Heading = () => {
             <h1>Projects
               <Image src={'images/stickers/hero/code.svg'} width={"40px"}/>
             </h1>
-            <p>I've built cool apps and websites using anything from HTML to React (and even PHP!). Here are some of my favorite projects over the course of my journey.</p>
+            <p>I've built cool apps and websites using anything from HTML to React. Here are some of my favorite projects over the course of my journey.</p>
           </Col>
         </Row>
       </Container>
